b61217c8caf6c8d9ccc73e4e5fc0a612199ab4bc
Gentjana <hanigentjana@gmail.com>
2026-01-20 16:31:40 +0100

    adds_updates

--- src/main/java/gentjanahani/Application.java
+++ src/main/java/gentjanahani/Application.java
@@ -3,6 +3,7 @@ package gentjanahani;
 import gentjanahani.dao.EventoDAO;
 import gentjanahani.entities.Evento;
 import gentjanahani.entities.EventoType;
+import gentjanahani.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -22,12 +23,31 @@ public class Application {
 Evento compleanno=new Evento("Dicciottesimo Martini", LocalDate.of(2026,3,8), "Torta a mezzannotte, tovagliato di carta", EventoType.PRIVATO, 50);
 Evento cenaAziendale=new Evento("cena aziendale Epicode", LocalDate.of(2026, 7,22),"Cena di fine anno, menù a buffet", EventoType.PUBBLICO, 300 );
 Evento pasqua=new Evento("pranzo di pasqua", LocalDate.of(2026,4,5), "Menù fisso, sono anocra aperte le prenotazioni", EventoType.PUBBLICO, 150);
-
+        Evento natale=new Evento("pranzo di Natale", LocalDate.of(2025,12,25), "Menù fisso, sono ancora aperte le prenotazioni", EventoType.PUBBLICO, 150);
 //------------------------------------SAVE--------------------------
-evento.save(matrimonio);
-evento.save(compleanno);
-evento.save(cenaAziendale);
-evento.save(pasqua);
+//evento.save(matrimonio);
+//evento.save(compleanno);
+//evento.save(cenaAziendale);
+//evento.save(pasqua);
+//        evento.save(natale);
 
+        //----------------------GET BY ID-------------------
+
+        try{
+            Evento eventoCercato=evento.getById(2);
+            System.out.println(eventoCercato);
+        }catch(NotFoundException exception){
+            System.out.println(exception.getMessage());
+        }
+
+
+
+        //-------------------------DELETE-------------------
+try{
+    evento.delete(4);
+
+}catch(NotFoundException exception){
+    System.out.println(exception.getMessage());
+}
     }
 }
--- src/main/java/gentjanahani/dao/EventoDAO.java
+++ src/main/java/gentjanahani/dao/EventoDAO.java
@@ -1,6 +1,7 @@
 package gentjanahani.dao;
 
 import gentjanahani.entities.Evento;
+import gentjanahani.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
@@ -27,5 +28,31 @@ transaction.commit();
 
         // 5. Log di avvenuto salvataggio
         System.out.println("L'evento " + newEvento.getTitolo()+ " è stato salvato corretamente.");
+    }
+
+    public Evento getById(long idEvento){
+        Evento found=entityManager.find(Evento.class, idEvento);
+        if(found == null) throw new NotFoundException(idEvento);
+        return found;
+    }
+
+    public void delete(long idEvento){
+        // 1. Cerco l'evento tramite id nel DB
+        Evento found=this.getById(idEvento);
+
+        // 2. Chiediamo all'EntityManager di creare una nuova transazione
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        // 3. Facciamo partire la transazione
+        transaction.begin();
+
+        // 4. Rimuoviamo  l'oggetto in questione tramite metodo .remove()
+        entityManager.remove(found);
+
+        // 5.  COMMIT della transazione
+        transaction.commit();
+
+        // 6. Log di avvenuta cancellazione
+        System.out.println("L' evento con id: " + idEvento +  " è stato eliminato correttamente!");
     }
 }
